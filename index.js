117acc255682b86e184c2f04b11c5285790288a1
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-12 12:40:48 -0500

    commit tree 4632d9f3eab5e71295780132fa70c1eec1895a33

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(area) {
+  if(area > 42) {
+      return area-42; 
+  } else {
+      return 42-area;
+  }
+    
+}
+function distanceFromHqInFeet(street) {
+    if(street > 42) {
+        return (street - 42) * 264; 
+    } else {
+        return (42 - street) * 264;
+    }
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    return [Math.abs(num1 - num2)] * 264}
+
+
+    function calculatesFarePrice(start, end) {
+        const distanceTravelled = distanceTravelledInFeet(start, end); 
+        if (distanceTravelled < 400) {
+          return 0;
+        } else if (distanceTravelled >= 400 && distanceTravelled < 2000) {
+          return (distanceTravelled - 400) / 50;
+        }
+        else if ((distanceTravelled >= 2000) && (distanceTravelled <= 2500)) {
+          return 25;
+        }
+        else {
+          return 'cannot travel that far';
+        }
+      }
